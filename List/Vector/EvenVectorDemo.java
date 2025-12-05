678caaf03aad88371fa6f5ee5b53456dfb8bbc7b
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2025-12-05 11:17:39 +0530

    Collection

---
+++ List/Vector/EvenVectorDemo.java
@@ -0,0 +1,33 @@
+/*Q1. Write a Java program to store 5 integers in a Vector.
+ Print only the even numbers using a loop.
+Vector creation
+Adding elements
+Logical condition: num % 2 == 0 */
+//  WAP to store 5 integer values in Collection or Vector and calculate its sum.
+import java.util.*;
+
+class EvenVectorDemo{
+	
+	public static void main(String x[]){
+		
+		Vector v = new Vector();
+		v.add(1);
+		v.add(2);
+		v.add(3);
+		v.add(4);
+		v.add(5);
+		
+		Iterator i = v.iterator();
+		int sum =0;
+		while(i.hasNext()){
+			
+			Object obj = i.next();
+			
+			String str = (int)obj % 2==0 ? "Even" : "Odd";
+			System.out.println(str);
+				
+		}
+		//System.out.println("sum of all vallues " +sum);
+		
+	}
+}
