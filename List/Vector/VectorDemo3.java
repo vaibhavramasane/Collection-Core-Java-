e7e1a87c993860c98ccaab256f5498064a46bf10
vaibhavramasane <125948318+vaibhavramasane@users.noreply.github.com>
2025-12-05 11:15:46 +0530

    :Collection

---
+++ List/Vector/VectorDemo3.java
@@ -0,0 +1,24 @@
+//
+import java.util.*;
+class VectorDemo3{
+	
+	public static void main(String x[]){
+		
+		Vector v = new Vector();
+		v.add(10);
+		v.add(20);
+		v.add(30);
+		v.add(40);
+		v.add(50);
+		Iterator i = v.iterator();
+		
+		while(i.hasNext()){
+			
+			
+			Object obj = i.next();
+			
+			System.out.println(obj);
+			
+		}
+	}
+}
